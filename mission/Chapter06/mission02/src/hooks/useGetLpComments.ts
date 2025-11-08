e11738238fb3a8ed7cc1f829b89ff5990f2c0b4d
wupe1001 <sangwu1104@naver.com>
2025-11-08 23:32:33 +0900

    "mission : 댓글 정렬 order 변경 시 깜빡임 개선 피드백 반영"
    This reverts commit a10ae7de6b6d8a83d5b78b138379e5c4c93055b0.

--- mission/Chapter06/mission02/src/hooks/useGetLpComments.ts
+++ mission/Chapter06/mission02/src/hooks/useGetLpComments.ts
@@ -4,7 +4,7 @@ import { QUERY_KEY } from "../constants/key";
 
 const useGetLpComments = (lpid: string, order: "asc" | "desc") => {
   return useInfiniteQuery({
-    queryKey: [QUERY_KEY.comments, lpid, order],
+    queryKey: [QUERY_KEY.comments, lpid, {order}],
     queryFn: ({ pageParam }) =>
       getLpComments({ lpid, order, cursor: pageParam }),
     initialPageParam: 0,
