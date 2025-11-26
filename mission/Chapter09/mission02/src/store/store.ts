14319257f752f7cc7e8beb1e4e0aad9f1b7a17c5
wupe1001 <sangwu1104@naver.com>
2025-11-26 17:28:09 +0900

    mission : 9주차 미션 2 수행

---
+++ mission/Chapter09/mission02/src/store/store.ts
@@ -0,0 +1,22 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { cartReducer } from "../slices/cartSlice";
+import { modalReducer } from "../slices/modalSlice";
+
+function createStore() {
+  const store = configureStore({
+    reducer: {
+      cart: cartReducer,
+      modal: modalReducer,
+    },
+  });
+
+  return store;
+}
+
+// store 내보내기
+const store = createStore();
+
+export default store;
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
